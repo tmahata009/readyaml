540194507f0a7adfe1eb31e6e10b24bf3a03ac9c
Tapan <tmahata009@gmail.com>
2019-06-20 11:39:39 +0530

    this is sample inheritance program example in python

---
+++ inheritace.py
@@ -0,0 +1,38 @@
+class A:
+    def __init__(self):
+        print("in init A")
+
+    def feature1(self):
+        print("feature1 is working")
+
+    def feature2(self):
+        print("feature2 is working")
+
+
+class B:
+    def __init__(self):
+        print("init in B")
+
+
+    def feature3(self):
+        print("feature3 is working")
+
+    def feature4(self):
+        print("feature4 is working")
+class C(A,B):
+    def __init__(self):
+
+        print("init in C")
+
+
+    def feature5(self):
+        print("feature3 is working")
+
+    def feature6(self):
+        print("feature4 is working")
+
+
+
+c = C()
+
+c.feature1()
